8bbe4d6eb200de3719e367cf1f012a1fc2cb3f5b
Michel Bystranowski <michel@vertige.org>
2013-07-25 09:49:57 +0200

    commit initial - généré par la Fabrique

---
+++ lang/paquet-chartetypo_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'chartetypo_description' => '',
+	'chartetypo_nom' => 'Charte Typographique',
+	'chartetypo_slogan' => 'Des pages pour visualiser d\'un coup les styles d\'un site Z',
+);
+
+?>
